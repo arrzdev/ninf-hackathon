0f8f0f1ab665ac0c2a0614bfdbf5605a1d36d88a
arrzdev <andre.arroz.santos@gmail.com>
2023-05-12 22:20:43 +0100

    Done script to scrape beach data

---
+++ scripts/beach-data.py
@@ -0,0 +1,96 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+
+NOS_VALUE = 1.852
+
+def parse_coords(script_tag):
+  script_tag = str(script_tag)
+  sindex = script_tag.index("Dec2DMS(")
+  eindex = script_tag.index(")", sindex)
+  return script_tag[sindex+8:eindex].strip()
+
+def parse_direction(src_tag):
+  src_tag = str(src_tag)
+  sindex = src_tag.rfind("_")
+  eindex = src_tag.index(".gif", sindex)
+  return src_tag[sindex+1:eindex].strip().upper()
+  
+class Beach:
+  def __init__(self, beach_id=14):
+    self.url = f"https://www.ipma.pt/pt/maritima/costeira/index.jsp?selLocal={beach_id}&idLocal={beach_id}"
+    self.data = self.__get_beach_data__()
+
+  def __get_beach_data__(self):
+    req = requests.get(self.url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+
+    beach_data = soup.find("div", attrs={"class":"marbottom10"})
+
+    #metadata
+    beach_name = beach_data.find("strong").text.strip()
+    beach_lat = parse_coords(beach_data.find_all("em")[0])
+    beach_lon = parse_coords(beach_data.find_all("em")[1])
+    beach_alt = beach_data.find_all("em")[2].text.split()[0].strip()
+
+    beach_metadata = {
+      "name": beach_name,
+      "lat": beach_lat,
+      "lon": beach_lon, 
+      "height": beach_alt
+    }
+
+    #get today date and the next 3 days
+    dates = []
+    for i in range(4):
+      dates.append((datetime.now() + timedelta(days=i)).strftime("%d-%m-%Y"))
+
+    predicted_data = soup.find_all("div", attrs={"class":"simpleTabsContent"})
+
+    beach_states = []
+    for i, day_data in enumerate(predicted_data):
+      #get the data divided in chunks of hours
+      hour_states = day_data\
+        .find_all("tr")[1:]
+        
+      #handle first hour outside of table
+      base_state = day_data.find_all("td")[0:11]
+      hour_states.insert(0, None)      
+
+      for hour_state in hour_states:
+        hour_data = hour_state.find_all("td") if hour_state != None else base_state
+        beach_states.append({
+          "date": dates[i],
+          "hour": hour_data[0].text.strip(),
+          "water_depth": float(hour_data[1].text.strip()),
+          "wave_height": float(hour_data[2].text.strip()),
+          "wave_direction": parse_direction(hour_data[3].find("img")["src"]),
+          "wave_period": float(hour_data[4].text.strip()),
+          "max_wave_period": float(hour_data[5].text.strip()),
+          "wind_speed": float(hour_data[6].text.strip()) * NOS_VALUE,
+          "wind_direction": parse_direction(hour_data[7].find("img")["src"]),
+          "beaufort_scale": int(hour_data[8].text.strip()),
+          "water_temperature": float(hour_data[9].text.strip()),
+          "power": float(hour_data[10].text.strip())          
+        })
+
+    return {
+      "metadata": beach_metadata,
+      "states": beach_states
+    }
+
+  #public getters
+  def get_data(self):
+    return self.data
+  
+  def get_states_by_day(self, day_offset=0):
+    max_offset = min((day_offset+1)*24, len(self.data["states"]))
+    return self.data["states"][day_offset*24:max_offset]
+  
+  def get_average(self, property, day_offset=0):
+    states = self.get_states_by_day(day_offset)
+    return sum([float(state[property]) for state in states])/len(states)
+
+
+# caparica = Beach(14)
+# print(caparica.get_data()["states"][0])
