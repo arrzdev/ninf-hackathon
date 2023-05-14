ba68c2f0a1a1b68363b0b539d6ad57638e637c11
arrzdev <andre.arroz.santos@gmail.com>
2023-05-14 11:00:09 +0100

    Added jdn to next-config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-    reactStrictMode: true,
-};
-=======
   reactStrictMode: true,
   images: {
     domains: ['static.globalnoticias.pt']
   }
 }
->>>>>>> 23624c5 (Added news Feed)
 
 module.exports = nextConfig;
